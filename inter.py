1fe490a0a5e2c1ee2835861532c6a59477b9d089
Serhii Levytskyi <levytskyi.sv@gmail.com>
2023-11-07 07:26:16 +0100

    bubble

--- inter.py
+++ inter.py
@@ -79,6 +79,20 @@ class A:
         self.name = name
 
 
+# Napsat program, který spustí algoritmus Bubble sort?
+def bubble_sort(list):
+    n = len(list)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if list[j] > list[j + 1]:
+                list[j], list[j + 1] = list[j + 1], list[j]
+    return list
+
+
+list = [64, 34, 25, 12, 22, 11, 90]
+sorted_list = bubble_sort(list)
+print("Original List: ", list)
+print("Sorted List: ", sorted_list)
 # a = A("K")
 # a = A()
 # print(a.name)
